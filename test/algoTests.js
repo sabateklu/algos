f8b81270765a7fb01ba9b6325c26f458ea868b98
Saba Teklu <saab.teklu@gmail.com>
2021-03-31 14:43:22 -0600

    added mocha tests for isAnagram function

--- test/algoTests.js
+++ test/algoTests.js
@@ -9,4 +9,20 @@ describe('time conversion function', () => {
   it('function should return military time for PM', () => {
     expect(algo.timeConversion('03:04:23PM')).to.equal('15:04:23');
   });
+});
+
+describe('isAnagram function', ()=> {
+  it('should return if two words are anagrams of each other', () => {
+    expect(algo.isAnagram('dog', 'god')).to.equal(true);
+    expect(algo.isAnagram('cat', 'tar')).to.equal(false);
+  });
+  it('should return true even if one word contains non-alpahbetic characters', () => {
+    expect(algo.isAnagram('dog$ $9?', 'god')).to.equal(true);
+  });
+  it('should return true for strings that contain the same letter varying in upper/lower case', () => {
+    expect(algo.isAnagram('Dog', 'dog')).to.equal(true);
+  });
+  it('shou;d return false if one string contains all and more letters than the other',() => {
+    expect(algo.isAnagram('mouse', 'useoml')).to.equal(false);
+  })
 })
